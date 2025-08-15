6bd8c8b465c66cbf8c8614130e7250a01cbf3500
trendingeswari <trendingeswari@gmail.com>
2025-08-16 00:32:43 +0530

    Spring Boot Project

--- spring-data-jpa-mapping/src/main/java/com/tnsif/spring_data_jpa_mapping/controller/OrderController.java
+++ spring-data-jpa-mapping/src/main/java/com/tnsif/spring_data_jpa_mapping/controller/OrderController.java
@@ -1,10 +1,21 @@
 package com.tnsif.spring_data_jpa_mapping.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+import com.tnsif.spring_data_jpa_mapping.controller.dto.OrderRequest;
+import com.tnsif.spring_data_jpa_mapping.controller.dto.OrderResponse;
+import com.tnsif.spring_data_jpa_mapping.controller.dto.entity.Customer;
+import com.tnsif.spring_data_jpa_mapping.controller.dto.entity.repository.CustomerRepository;
+import com.tnsif.spring_data_jpa_mapping.controller.dto.entity.repository.ProductRepository;
 
 @RestController
 public class OrderController {
 	 @Autowired
+	 
 	    private CustomerRepository customerRepository;
 	    @Autowired
 	    private ProductRepository productRepository;
